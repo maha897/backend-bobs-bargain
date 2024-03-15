ce42bd704dd27a159dc82de8f116dbb8c72a1ba8
msg <mats.gravem@no.experis.com>
2024-03-15 09:53:55 +0100

    Initial Commit

---
+++ src/test/java/co/booleanuk/bargain/ApplicationTests.java
@@ -0,0 +1,10 @@
+package co.booleanuk.bargain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+	@Test
+	void contextLoads() {}
+}
